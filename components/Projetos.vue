bba892dc69867dc68aa0a4d3e4f5261c03b11ca8
iDz <bdls1704@hotmail.com>
2023-07-06 21:19:23 -0300

    update projects

--- components/Projetos.vue
+++ components/Projetos.vue
@@ -13,7 +13,11 @@
         <div>
             <Swiper
                 :slides-per-view="perView"
-                :space-between="50"
+                :coverflow-effect="{
+                    scale: 0.8,
+                    modifier: 1,
+                    slideShadows: true,
+                }"
                 :modules="modules"
                 pagination
                 grabCursor
@@ -23,13 +27,14 @@
             >
                 <SwiperSlide v-for="(project, index) in projects" :key="project.name">
                     <div
-                        class="p-4 md:p-20 flex gap-10 flex-col lg:flex-row items-center justify-between rounded-xl"
-                        :class="{ 'lg:flex-row-reverse': index % 2 === 0 }"
+                        class="p-4 md:p-10 flex items-center justify-between rounded-xl bg-neutral-800/70 hover:bg-neutral-900/70 duration-200"
+                        :class2="{ 'lg:flex-row-reverse': index % 2 === 0 }"
                     >
                         <div class="flex flex-col gap-4">
-                            <p class="uppercase text-neutral-500 text-sm">{{ project.type }}</p>
-
-                            <TextoAnimado class="text-2xl font-bold" :label="project.name" />
+                            <div>
+                                <p class="uppercase text-neutral-500 text-sm">{{ project.type }}</p>
+                                <TextoAnimado class="text-2xl font-bold" :label="project.name" />
+                            </div>
 
                             <p>{{ project.description }}</p>
 
@@ -51,11 +56,6 @@
                                 </a>
                             </div>
                         </div>
-                        <img
-                            :src="project.image"
-                            :alt="`Imagem do projeto '${project.name}'`"
-                            class="w-1/2 h-72 object-cover rounded-xl hidden lg:block"
-                        />
                     </div>
                 </SwiperSlide>
             </Swiper>
@@ -83,51 +83,94 @@ const perView = computed(() => {
 
 const projects = [
     {
-        type: "Aplicação de e-commerce",
-        name: "Crown Clothing 1",
+        type: "Aplicação full stack",
+        name: "Runners Club",
         description:
-            "Online store built with React and Firebase. It has a complete authentication system, shopping cart, payment processing and more.",
-        image: "https://picsum.photos/500/500",
-        technologies: ["React", "NodeJS", "MongoDB", "Express", "Firebase"],
+            "Plataforma para uma empresa especializada em eventos e assessorias de corrida personalizada. Conta com informativos, calendário de eventos, autenticação de usuários, loja virtual e muito mais.",
+        technologies: [
+            "Typescript",
+            "Vue",
+            "Quasar",
+            "NodeJS",
+            "NestJS",
+            "Prisma",
+            "PostgreSQL",
+            "Supabase",
+            "Vercel",
+            "Docker",
+        ],
+        links: [{ name: "Runners Club", link: "https://www.oficialrunnersclub.com.br/" }],
+    },
+
+    {
+        type: "Aplicação full stack + aplicativo nativo windows",
+        name: "MTA Launcher",
+        description: `
+            Um aplicativo permite que os usuários tenham uma melhor experiência ao buscar por servidores de um jogo online.
+            A aplicação conta com landing page, sistema de gerenciamento de perfis, etc.
+        `,
+        technologies: [
+            "Typescript",
+            "React",
+            "NextJS",
+            "MaterialUI",
+            "NodeJS",
+            "Express",
+            "Prisma",
+            "Planetscale",
+            "MySQL",
+            "Docker",
+            "Tauri",
+        ],
         links: [
-            { name: "Ver o código", link: "" },
-            { name: "Visitar o app", link: "" },
+            { name: "Landing Page", link: "https://www.mta-launcher.com/" },
+            { name: "Landing Page (Código)", link: "https://github.com/brenodanyel/mta-launcher-landing-page" },
+            { name: "Painel", link: "https://app.mta-launcher.com/" },
+            { name: "Painel (Código)", link: "https://github.com/brenodanyel/mta-launcher-painel" },
+            { name: "Back End", link: "https://backend-mta-launcher.vercel.app/" },
+            { name: "Back End (Código)", link: "https://github.com/brenodanyel/mta-launcher-backend" },
         ],
     },
+
     {
-        type: "Aplicação de e-commerce",
-        name: "Crown Clothing 2",
-        description:
-            "Online store built with React and Firebase. It has a complete authentication system, shopping cart, payment processing and more.",
-        image: "https://picsum.photos/500/500",
-        technologies: ["React", "NodeJS", "MongoDB", "Express", "Firebase"],
+        type: "Front End",
+        name: "Search Devs",
+        description: `Aplicação web que permite que usuários busquem perfis de desenvolvedores cadastrados no github.
+            É possível visualizar informações pessoais, tecnologias utilizadas, projetos ativos, localização e muito mais.`,
+        technologies: ["Typescript", "React", "Material UI"],
         links: [
-            { name: "Ver o código", link: "" },
-            { name: "Visitar o app", link: "" },
+            { name: "Search Devs", link: "https://search-devs-omega.vercel.app/" },
+            { name: "Search Devs (Código)", link: "https://github.com/brenodanyel/search_devs" },
         ],
     },
+
     {
-        type: "Aplicação de e-commerce",
-        name: "Crown Clothing 3",
-        description:
-            "Online store built with React and Firebase. It has a complete authentication system, shopping cart, payment processing and more.",
-        image: "https://picsum.photos/500/500",
-        technologies: ["React", "NodeJS", "MongoDB", "Express", "Firebase"],
+        type: "API REST",
+        name: "MTA-SA API",
+        description: `Uma API rest que permite que usuários busquem informações sobre servidores de um jogo online.`,
+        technologies: ["Typescript", "NodeJS", "Express"],
+        links: [{ name: "MTA-SA API (Código)", link: "https://github.com/brenodanyel/mtasa-api" }],
+    },
+
+    {
+        type: "Front end app",
+        name: "BrenoFlix",
+        description: `Uma aplicação web que simula uma plataforma de streaming de vídeos. Inspirada na interface da Netflix.`,
+        technologies: ["Typescript", "React"],
         links: [
-            { name: "Ver o código", link: "" },
-            { name: "Visitar o app", link: "" },
+            { name: "BrenoFlix", link: "https://breno-cloneflix.vercel.app/" },
+            { name: "BrenoFlix (Código)", link: "https://github.com/brenodanyel/netflix-clone" },
         ],
     },
+
     {
-        type: "Aplicação de e-commerce",
-        name: "Crown Clothing 4",
-        description:
-            "Online store built with React and Firebase. It has a complete authentication system, shopping cart, payment processing and more.",
-        image: "https://picsum.photos/500/500",
-        technologies: ["React", "NodeJS", "MongoDB", "Express", "Firebase"],
+        type: "Front end app",
+        name: "Random User",
+        description: `Uma aplicação web que exibe informações sobre usuários aleatórios. Utiliza a API pública Random User.`,
+        technologies: ["Typescript", "React", "Typescript"],
         links: [
-            { name: "Ver o código", link: "" },
-            { name: "Visitar o app", link: "" },
+            { name: "Random User", link: "https://random-user-sand-five.vercel.app/" },
+            { name: "Random User (Código)", link: "https://github.com/brenodanyel/random-user" },
         ],
     },
 ];

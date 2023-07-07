c4e3be187e870c0620440db2db89c9c46e216968
= <=>
2023-07-07 09:32:54 -0300

    fix padding

--- pages/index.vue
+++ pages/index.vue
@@ -6,19 +6,19 @@
             </div>
         </section>
 
-        <section class="py-36 bg-neutral-800">
+        <section class="py-12 bg-neutral-800">
             <div class="px-4 md:px-12 lg:px-28 xl:px-36">
                 <Habilidades data-aos="fade-up" />
             </div>
         </section>
 
-        <section class="py-36 bg-neutral-900">
+        <section class="py-12 bg-neutral-900">
             <div class="px-4 md:px-12 lg:px-28 xl:px-36">
                 <Projetos data-aos="fade-up" />
             </div>
         </section>
 
-        <section class="py-36 bg-neutral-800">
+        <section class="py-12 bg-neutral-800">
             <div class="px-4 md:px-12 lg:px-28 xl:px-36">
                 <Contato id="contato" data-aos="fade-up" />
             </div>
